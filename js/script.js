e819388109b7d0b6bd96e8c4915a0bc6ef4ee942
SYAN <syanays.git@gmail.com>
2024-01-18 11:38:09 +0300

    prevent submitting form if pw invalid

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
 // DOM Variables
 
+const form = document.querySelector('form')
 const pw = document.getElementById('pw')
 const pwConfirm = document.getElementById('pw-confirm')
 const errorMsg = document.getElementById('error')
 
 // Event Listeners
 
-pw.addEventListener('keyup', matchPassword)
-pwConfirm.addEventListener('keyup', matchPassword)
+pw.addEventListener('input', matchPassword)
+pwConfirm.addEventListener('input', matchPassword)
+form.addEventListener('submit', matchPassword)
 
-// Functions
+// Function
 
-function matchPassword() {
+function matchPassword(event) {
     if (pw.value != pwConfirm.value) {
-        errorMsg.textContent = '* Passwords do not match'
+        errorMsg.textContent = '* Passwords do not match';
+        event.preventDefault();
         return false;
     } else {
         errorMsg.textContent = '';
